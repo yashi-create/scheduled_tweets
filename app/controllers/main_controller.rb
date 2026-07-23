98d983ab5f33e9ce6268c5263a7b65afe0535f5c
Yashi Sharma <84683011+yashi-create@users.noreply.github.com>
2026-07-23 17:00:28 +0530

    adding flashes (#5)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
   def index
+    flash[:notice] = "Logged in successfully" # feature of rails controller
+    flash[:alert] = "Invalid credentials"
   end
 end
